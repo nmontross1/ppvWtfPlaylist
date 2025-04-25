139d258e804084fe31c456aaa6cb54f0be4f5437
nmontross1 <n.montross1@gmail.com>
2025-04-25 12:49:25 -0400

    Update M3U file

--- src/services/json-to-m3u-service.js
+++ src/services/json-to-m3u-service.js
@@ -1,15 +1,13 @@
 export default class JsonToM3uService {
-  convert(json) {
-    if (!json?.streams) return "";
+  convert(enrichedStreams) {
+    if (!Array.isArray(enrichedStreams) || enrichedStreams.length === 0) return "";
 
     let m3u = "#EXTM3U\n";
 
-    json.streams.forEach((category) => {
-      category.streams.forEach((stream) => {
-        const { name, tag, poster, iframe, category_name } = stream;
-        m3u += `#EXTINF:-1 tvg-id="${tag}" tvg-name="${name}" tvg-logo="${poster}" group-title="${category_name}",${name}\n`;
-        m3u += `${iframe}\n`;
-      });
+    enrichedStreams.forEach((stream) => {
+      const { name, tag, poster, m3u8, category_name } = stream;
+      m3u += `#EXTINF:-1 tvg-id="${tag}" tvg-name="${name}" tvg-logo="${poster}" group-title="${category_name}",${name}\n`;
+      m3u += `${m3u8}\n`;
     });
 
     return m3u;
