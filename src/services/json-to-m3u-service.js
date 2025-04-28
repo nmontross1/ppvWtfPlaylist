5c207178913e5867d4e1f00cb6b65cccd2dbab7e
nmontross1 <n.montross1@gmail.com>
2025-04-28 12:06:59 -0400

    Update M3U file

--- src/services/json-to-m3u-service.js
+++ src/services/json-to-m3u-service.js
@@ -2,18 +2,16 @@ export default class JsonToM3uService {
   async convert(jsonData) {
     const headerString = '|Referer=https://ppv.wtf/&User-Agent=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F123.0.0.0%20Safari%2F537.36';
     const lines = ['#EXTM3U'];
-  
+
     for (const item of jsonData) {
-      if (item.streams && item.streams.length > 0) {
-        const streamDetail = item.streams[0];
-        const m3u8Url = streamDetail.stream_url + headerString;
-        const extinf = `#EXTINF:-1 tvg-id="${item.league}" tvg-name="${item.name}" tvg-logo="${item.icon}" group-title="${item.league}",${item.name}`;
+      if (item.m3u8) {
+        const m3u8Url = item.m3u8 + headerString;
+        const extinf = `#EXTINF:-1 tvg-id="${item.tag}" tvg-name="${item.name}" tvg-logo="${item.poster}" group-title="${item.category_name}",${item.name}`;
         lines.push(extinf);
         lines.push(m3u8Url);
       }
     }
-  
+
     return lines.join('\n');
   }
-  
 }
