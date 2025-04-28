30284bf4c4c8323cc59f748b77644cb752250610
nmontross1 <n.montross1@gmail.com>
2025-04-28 11:07:26 -0400

    extra line after url

--- src/services/json-to-m3u-service.js
+++ src/services/json-to-m3u-service.js
@@ -9,7 +9,7 @@ export default class JsonToM3uService {
       const cleanM3u8 = m3u8.replace(/\\\//g, "/");
 
       m3u += `#EXTINF:-1 tvg-id="${tag}" tvg-name="${name}" tvg-logo="${poster}" group-title="${category_name}",${name}\n`;
-      m3u += `${cleanM3u8}\n`;
+      m3u += `${cleanM3u8}\n\n`;
     });
 
     return m3u;
