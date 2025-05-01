16a74dd2fd89df818462e5869460264c5fc3ea58
nmontross1 <n.montross1@gmail.com>
2025-05-01 09:32:35 -0400

    Update M3U file

--- src/services/json-to-m3u-service.js
+++ src/services/json-to-m3u-service.js
@@ -1,15 +1,11 @@
 export default class JsonToM3uService {
   async convert(jsonData) {
-    const referer = 'https://ppv.wtf/';
-    const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36';
     const lines = ['#EXTM3U'];
 
     for (const item of jsonData) {
       if (item.m3u8) {
         const extinf = `#EXTINF:-1 tvg-id="${item.tag}" tvg-name="${item.name}" tvg-logo="${item.poster}" group-title="${item.category_name}",${item.name}`;
         lines.push(extinf);
-        lines.push(`#EXTVLCOPT:http-referrer=${referer}`);
-        lines.push(`#EXTVLCOPT:http-user-agent=${userAgent}`);
         lines.push(item.m3u8);
       }
     }
@@ -17,4 +13,3 @@ export default class JsonToM3uService {
     return lines.join('\n');
   }
 }
-
