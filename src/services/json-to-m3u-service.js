1f4cab6e8f25ffbe246648baa10b09a648f7d6ee
nmontross1 <n.montross1@gmail.com>
2025-05-21 15:08:26 -0400

    Update M3U file

--- src/services/json-to-m3u-service.js
+++ src/services/json-to-m3u-service.js
@@ -1,6 +1,6 @@
 export default class JsonToM3uService {
   async convert(jsonData) {
-    const lines = ['#EXTM3U'];
+    const lines = ['#EXTM3U\n'];
 
     for (const item of jsonData) {
       if (item.m3u8) {
@@ -8,7 +8,7 @@ export default class JsonToM3uService {
         lines.push('#EXTVLCOPT:http-referrer=https://ppv.wtf/');
         lines.push('#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:138.0) Gecko/20100101 Firefox/138.0');
 
-        const extinf = `#EXTINF:-1 tvg-id="${item.tag}" tvg-name="${item.name}" tvg-logo="${item.poster}" group-title="${item.category_name}",${item.name}`;
+        const extinf = `#EXTINF:-1 tvg-id="${item.tag}" tvg-name="${item.name}" tvg-logo="${item.poster}" group-title="${item.category_name}",${item.name}\n`;
         lines.push(extinf);
         lines.push(item.m3u8);
       }
