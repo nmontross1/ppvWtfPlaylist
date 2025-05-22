99bceb3418063229fab4e2f224d3d7f0c71e412a
nmontross1 <n.montross1@gmail.com>
2025-05-22 12:48:56 -0400

    we want all m3u8

--- src/services/json-to-m3u-service.js
+++ src/services/json-to-m3u-service.js
@@ -3,7 +3,6 @@ export default class JsonToM3uService {
     const lines = ["#EXTM3U"];
 
     for (const item of jsonData) {
-      if (item.m3u8) {
         const extinf = `\n#EXTINF:-1 tvg-id="${item.tag}" tvg-name="${item.name}" tvg-logo="${item.poster}" group-title="${item.category_name}",${item.name}`;
         lines.push(extinf);
         lines.push("#EXTVLCOPT:http-origin=https://ppv.wtf");
@@ -11,8 +10,7 @@ export default class JsonToM3uService {
         lines.push(
           "#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:138.0) Gecko/20100101 Firefox/138.0",
         );
-        lines.push(item.m3u8);
-      }
+        lines.push(item?.m3u8);
     }
 
     return lines.join("\n");
