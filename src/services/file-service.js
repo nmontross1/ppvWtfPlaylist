1f052b5d7443b258c4eda4513f19e3214e6d67de
nmontross1 <n.montross1@gmail.com>
2025-05-22 09:54:20 -0400

    Update M3U file

--- src/services/file-service.js
+++ src/services/file-service.js
@@ -2,8 +2,8 @@ import fs from "fs";
 import path from "path";
 
 export default class FileService {
-  saveM3uToProjectFolder(m3uContent, filename = "playlist.m3u") {
-    const dir = path.resolve(process.cwd(), "m3uFile");
+  saveM3uToProjectFolder(m3uContent, filename = "playlist.m3u8") {
+    const dir = path.resolve(process.cwd(), "m3u8File");
 
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
