a57faf43c9ed9743e19063b9657015a8764c65c8
Nick Montross <nickmontross@MacBookPro.fios-router.home>
2025-08-11 14:11:40 -0400

    modify origin, referrer

--- src/services/json-to-m3u-service.js
+++ src/services/json-to-m3u-service.js
@@ -5,8 +5,8 @@ export default class JsonToM3uService {
     for (const item of jsonData) {
         const extinf = `\n#EXTINF:-1 tvg-id="${item.tag}" tvg-name="${item.name}" tvg-logo="${item.poster}" group-title="${item.category_name}",${item.name}`;
         lines.push(extinf);
-        lines.push("#EXTVLCOPT:http-origin=https://ppvs.su");
-        lines.push("#EXTVLCOPT:http-referrer=https://ppvs.su/");
+        lines.push("#EXTVLCOPT:http-origin=https://ppv.to");
+        lines.push("#EXTVLCOPT:http-referrer=https://ppv.to/");
         lines.push(
           "#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:138.0) Gecko/20100101 Firefox/138.0",
         );
